93291d21b67bf8fcc065f0e85671cf9c2d5aea36
Deserv0502 <1347475525@qq.com>
2020-08-27 17:36:49 +0800

    3.v

--- innisfree/JS/detile.js
+++ innisfree/JS/detile.js
@@ -1,16 +1,1 @@
-$(".detile_header").find("div").not(".HOME").click(function(e){
-    e.stopPropagation ? e.stopPropagation():e.cancelBubble = true;
-    for(let i = 0;i<$(".detile_header").find("div").length;i++){
-        $(".detile_header").find("div").eq(i).find("span").eq(0).css({"transform":"rotate(90deg)"});
-    }
-    $(".detile_header").find("ul").hide();
-    $(".detile_header").find("div").eq($(this).index()).find("span").eq(0).css({"transform":"rotate(-90deg)"});
-   $(".detile_header").find("ul").eq($(this).index()-1).show();
-})
-$(document).click(function(){
-    $(".detile_header").find("ul").hide();
-    for(let i = 0;i<$(".detile_header").find("div").length;i++){
-        $(".detile_header").find("div").eq(i).find("span").eq(0).css({"transform":"rotate(90deg)"});
-    }
-})
-MagniFier();
+"use strict";$(".detile_header").find("div").not(".HOME").click(function(e){e.stopPropagation?e.stopPropagation():e.cancelBubble=!0;for(var d=0;d<$(".detile_header").find("div").length;d++)$(".detile_header").find("div").eq(d).find("span").eq(0).css({transform:"rotate(90deg)"});$(".detile_header").find("ul").hide(),$(".detile_header").find("div").eq($(this).index()).find("span").eq(0).css({transform:"rotate(-90deg)"}),$(".detile_header").find("ul").eq($(this).index()-1).show()}),$(document).click(function(){$(".detile_header").find("ul").hide();for(var e=0;e<$(".detile_header").find("div").length;e++)$(".detile_header").find("div").eq(e).find("span").eq(0).css({transform:"rotate(90deg)"})}),MagniFier();
--- innisfree/JS/personal.js
+++ innisfree/JS/personal.js
@@ -1,7 +1,1 @@
-
-$(".person_username").html(localStorage.getItem("username"));
-$(".person_userid").html(localStorage.getItem("userid"));
-$(".person_exit").click(function(){
-    localStorage.clear();
-    location.reload();
-})
+"use strict";$(".person_username").html(localStorage.getItem("username")),$(".person_userid").html(localStorage.getItem("userid")),$(".person_exit").click(function(){localStorage.clear(),location.reload()});
